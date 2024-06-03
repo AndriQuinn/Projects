e21f18e34f27d8d2123b54a72dba57b8c836cbc9
AndriQuinn <160398457+AndriQuinn@users.noreply.github.com>
2024-06-03 23:29:16 +0800

    Update home.js

--- Ecommerce/EcoCommerce/home.js
+++ Ecommerce/EcoCommerce/home.js
@@ -6,8 +6,8 @@ console.log("Bat ang pogi ko?");
 console.log("Single po ako lmao");
 
 var banners =  {
-    1: "./NewAssets/rtx4090.jpg",
-    2: "./NewAssets/rtx4080.jpg",
+    1: "./Assets/rtx4090.jpg",
+    2: "./Assets/rtx4080.jpg",
 };
 
 var pos = 2;
