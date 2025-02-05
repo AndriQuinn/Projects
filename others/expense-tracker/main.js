5ccb6200c6a03af8c629c79597f5df59d4f4de63
AndriQuinn <160398457+AndriQuinn@users.noreply.github.com>
2025-02-05 10:40:09 +0800

    Update main.js

--- others/expense-tracker/main.js
+++ others/expense-tracker/main.js
@@ -1,11 +1,22 @@
-const dateNow = new Date().toString().split(" ");
-var day = document.getElementById("day");
-var week = document.getElementById("week");
-var month = document.getElementById("month");
-day.textContent = dateNow[2] + " 日";
-week.textContent = (Math.round(parseInt(dateNow[2]) / 7 +0.6)) + " 週";
-month.textContent = parseInt(new Date().getMonth()) + 1 + " 月";
 
+// Date Tracking
+const dateNow = new Date().toString().split(" ");
+var day = document.getElementsByClassName("day");
+var week = document.getElementsByClassName("week");
+var month = document.getElementsByClassName("month");;
+
+// Inject Spevified Dates
+// Allowance Board
+day[0].textContent = dateNow[2] + " 日";
+week[0].textContent= (Math.round(parseInt(dateNow[2]) / 7 +0.6)) + " 週";
+month[0].textContent= parseInt(new Date().getMonth()) + 1 + " 月";
+
+// Expense Board
+day[1].textContent  = dateNow[2] + " 日";
+week[1].textContent  = (Math.round(parseInt(dateNow[2]) / 7 +0.6)) + " 週";
+month[1].textContent  = parseInt(new Date().getMonth()) + 1 + " 月";
+
+// Menu Toggled
 var menuToggled = false;
 
 function toggleMenu() {
